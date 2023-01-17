8af12ae9d8102115cefda3f9c64a3b9e60b04ae7
Rommi666 <77491330+Roni7689@users.noreply.github.com>
2023-01-17 13:35:01 +0200

    MÖI

---
+++ Kiuas/Kiuas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//Roni
+namespace Kiuas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string asetus;
+            Console.WriteLine("Tervetuola Harvia kiukaan tietoihin");
+            kiuas Harvia = new kiuas(); // kiuas olion luonti
+            while (true) //while loop toimii niin kauan kunnes se breikataan
+            {
+                Console.WriteLine("Aseta kiuas päälle/pois: virta");
+                asetus = Console.ReadLine().ToLower(); //asetetaan stringi muuttuja pikku kirjaimin
+                if (asetus == "virta" && Harvia.on == true)
+                {
+                    break;
+                }
+                else Harvia.Päällä(); //käytetään kiuas luokan metodia, kiukaan päälle laittamiseen
+
+                Console.Write("aseta kiukaan arvot \n aseta lämpötila: ");
+               
+                Harvia.asetalampotila( int.Parse (Console.ReadLine())); //käytetään kiuasluokan metodia, käännetään string muuttuja int parsella
+                Console.Write("aseta kosteus: ");
+                Harvia.asetakosteus(int.Parse(Console.ReadLine()));
+                Console.WriteLine(Harvia.tiedot()); //tulostetaan kiukaan tiedot, käyttäen kiuas luokan metodia
+
+            }
+            Console.WriteLine("Kiitos Harvia kiukaan käytöstä ");
+        }
+    }
+}
---
+++ Kiuas/Kiuas/kiuas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kiuas
+{
+    class kiuas
+    {
+        string nimi = "Harvia";
+        int lampotila = 0;
+        int kosteus = 0;
+        public bool on = false;
+        string paalla;
+
+        public void asetalampotila(int i)
+        {
+            lampotila = i;
+
+
+        }
+
+        public void asetakosteus(int i)
+        {
+            kosteus = i;
+
+
+        }
+
+
+        public void Päällä()
+        {
+
+            if (on == true)
+            {
+                on = false;
+                paalla = "pois päältä";
+            }
+            else
+            {
+                on = true;
+                paalla = "päällä";
+            }
+
+        }
+        public string tiedot()
+        {            
+            string data = "kiukaan nimi: " + nimi + "\n kiukaanlampotila: " + lampotila + " celcius astetta\n kosteus: " + kosteus + "%\n kiuas: " + paalla;
+            return data;
+        }
+    }
+}
