6972b21fa09a77253143d15f0cfb7fb8c66e0272
Roni7689 <77491330+Roni7689@users.noreply.github.com>
2023-03-17 00:58:32 +0200

    kt 18

---
+++ MovieApi/MovieApi/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApi.Controllers
+{
+
+    [Route("api/[cpntroller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _dbContext;
+
+        public MoviesController(MovieContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    
+    // GET: api/Movies
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+
+    {
+
+        if (_dbContext.Movies == null)
+
+        {
+
+            return NotFound();
+
+        }
+
+        return await _dbContext.Movies.ToListAsync();
+
+    }
+  }
+
+}
+
