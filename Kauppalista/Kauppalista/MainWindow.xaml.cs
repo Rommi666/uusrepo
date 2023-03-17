5e9d12741f01a7d8c0a039597b7e6ce423dbe63b
Rommi666 <77491330+Roni7689@users.noreply.github.com>
2023-03-17 03:19:46 +0200

    kt 21

---
+++ Kauppalista/Kauppalista/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = checkBox.Content.ToString();
+            StockPanel.Children.Add(textBlock);
+        }
+
+        private void BuyButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (CheckBox checkBox in new CheckBox[] { CheckBox1, CheckBox2, CheckBox3 })
+            {
+                checkBox.IsChecked = false;
+            }
+            StockPanel.Children.Clear();
+        }
+    }
+}
